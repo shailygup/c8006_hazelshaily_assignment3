888af0cba80ad9ebf766e7b53cfac2275b7d9a93
Shaily Gupta <shaily.gupta@cloudreach.com>
2018-02-24 11:05:54 -0800

    FirstIteration

---
+++ IPS.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+threshold=3
+count=1
+
+#monitor the /var/log/security file for any failed authentication errors
+grep "authentication failure" /var/log/secure | awk '{ print $13 }' | cut -b7-  | sort | uniq -c
+grep 'Failed password' /var/log/secure* | grep sshd | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' | sort |
+
+
+
