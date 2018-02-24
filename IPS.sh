5928c82a3879f9724820122601a447846de416fd
Shaily Gupta <shaily.gupta@cloudreach.com>
2018-02-24 15:14:05 -0800

    Clarification 1

--- IPS.sh
+++ IPS.sh
@@ -3,7 +3,7 @@
 threshold=3
 count=1
 
-#monitor the /var/log/security file for any failed authentication errors
+#monitor the /var/log/security file for any failed authentication errors, one or the other
 grep "authentication failure" /var/log/secure | awk '{ print $13 }' | cut -b7-  | sort | uniq -c
 grep 'Failed password' /var/log/secure* | grep sshd | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' | sort |
 
