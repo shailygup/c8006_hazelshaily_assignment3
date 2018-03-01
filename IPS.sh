27140cd9bf929a6b1a6fdb9d055e5b16ac4dcdbc
Hazel Cha <hazel.cha@cloudreach.com>
2018-03-01 13:58:36 -0800

    testing if statement

--- IPS.sh
+++ IPS.sh
@@ -1,6 +1,4 @@
-
 #!/bin/sh
-
 
 echo Please specify the threshold:
 read threshold
@@ -16,10 +14,16 @@ loggedIPs=`grep -i 'Failed password'  $filetolog | grep sshd | awk '{print $11}'
 #loop through the IPs found with failed attempts and blocks at firewall and sets crontab job to expire rule after set amount of time
 for IP in $loggedIPs
 do
-    iptables -A INPUT -s $IP -j DROP
-    iptables -A OUTPUT -d $IP -j DROP
-    echo "iptables -D INPUT -s $IP -j DROP" | at now + $timeLimit
-    echo "iptables -D OUTPUT -d $IP -j DROP" | at now + $timeLimit
+    if [ -n "$timeLimit" ]; then
+    	iptables -A INPUT -s $IP -j DROP
+    	iptables -A OUTPUT -d $IP -j DROP
+    	#The time limit for blocking the IP. The default setting will be block indefinitely. 
+    	echo "iptables -D INPUT -s $IP -j DROP" | at now + $timeLimit
+    	echo "iptables -D OUTPUT -d $IP -j DROP" | at now + $timeLimit
+    else
+    	iptables -A INPUT -s $IP -j DROP
+    	iptables -A OUTPUT -d $IP -j DROP
+    fi
 done
 
 
