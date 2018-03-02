2a18b33ea27a58cc9ad15448561bb9abbfea4c48
Shaily Gupta <shailygupta@Shailys-MBP.hitronhub.home>
2018-03-01 21:50:24 -0800

    Trying something

--- IPS.sh
+++ IPS.sh
@@ -1,23 +1,33 @@
 #!/bin/sh
 
+#download inotidy-tools to download in the background
+dnf install inotify-tools -y
+
+#User Input Section
 echo Please specify the threshold:
 read threshold
 echo 'Please specify a time limit (ex 1 minutes):'
 read timeLimit
 
-# file=`tail -f /var/log/secure`
+#tail the secure log in the background and keep updating the secure.x
 filetolog=/var/log/secure
-
 #finds the IP of location failed ssh attempts and prints the number of failed attempts
-loggedIPs=`grep -i 'Failed password'  $filetolog | grep sshd | awk '{print $11}' | sort | uniq -c | awk -v count=$threshold '{if ($1 >= count) {print $2}}'`
+loggedIPs=$(grep -i 'Failed password'  $filetolog | grep sshd | awk '{print $11}' | sort | uniq -c | awk -v count=$threshold '{if ($1 >= count) {print $2}}'
 
-#loop through the IPs found with failed attempts and blocks at firewall and sets crontab job to expire rule after set amount of time
-for IP in $loggedIPs
-do
-	iptables -A INPUT -s $IP -j DROP
-	iptables -A OUTPUT -d $IP -j DROP
-	echo "iptables -D INPUT -s $IP -j DROP" | at now + 1 minutes
-	echo "iptables -D OUTPUT -d $IP -j DROP" | at now + 1 minutes
+#use inotifywait to monitor the secure log file to check for any new updates in the file
+while inotifywait -e modify $filetolog; do
+	#loop through the IPs found with failed attempts and blocks at firewall and sets crontab job to expire rule after set amount of time
+	for IP in $(echo $loggedIPs | tr ',' '\n')
+	do
+		echo $IP >> Bad.txt
+		# iptables -F
+		# iptables -A INPUT -s $IP -j DROP
+		# iptables -A OUTPUT -d $IP -j DROP
+		# if [["$timeLimit" != none]] then
+		# 	echo "iptables -D INPUT -s $IP -j DROP" | at now + $timeLimit
+		# 	echo "iptables -D OUTPUT -d $IP -j DROP" | at now + $timeLimit
+		# fi
+	done
 done
 
 
