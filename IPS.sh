c3a665801e9df3611fae4d1ebc6c23b8d95b797f
Hazel <hazelcha@outlook.com>
2018-03-03 10:09:33 -0800

    Try 12

--- IPS.sh
+++ IPS.sh
@@ -19,14 +19,14 @@ echo ' '
 # echo 'Please specify the threshold:'
 threshold=3
 # echo 'Please specify a time limit  (ex 1 or 2) or "n" for default:'
-timeLimit=1
+timeLimit=10
 # echo 'Please specify a time metric (ex minute or hours) or "n" for default:'
 # metric=minutes
 
 #flush the table
 iptables -F
 iptables -X
-
+rm Banned
 #tail the secure log in the background and keep updating the secure.x
 filetolog=/var/log/secure
 #finds the IP users who have attempted a failed login higher than the threshold specified
@@ -39,27 +39,32 @@ fi
 
 function timeLimitfun {
 		(sleep $timeLimit
-		iptables -D INPUT -s $IP -j DROP)
+		iptables -D INPUT -s $IP -j DROP
+		echo "IP unblocked")
 }
 
 #use inotifywait to monitor the secure log file to check for any new updates in the file
-tail -f $filetolog | while read l
+tail -f /var/log/secure | while read l
 do
 	#loop through the IPs found with failed attempts and blocks at firewall and sets crontab job to expire rule after set amount of time
 
-	loggedIPs=$(grep 'Failed password' $filetolog | grep sshd | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' | sort | uniq -c | awk -v count="$threshold" '{if ($1 >= count) {print $2}}')
-	echo loggedIPs
+	loggedIPs=$(grep 'Failed password' /var/log/secure | grep sshd | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' | sort | uniq -c | awk -v count=3 '{if ($1 >= count) {print $2}} ')
+
+	echo "$loggedIPs" >> Banned
 		#testing for how the IPs are displayed
 		# echo $IP >> Bad.txt
 		# iptables -A INPUT -s "$IP" -j DROP
 		# iptables -A OUTPUT -d "$IP" -j DROP
 
 
-		if [ ! -z $loggedIPs ]; then
-					iptables -A INPUT -s $loggedIPs -j DROP
-					echo " IP Block being established"
+		if [ ! -z "$loggedIPs" ]; then
+			for IP in $(echo "$loggedIPs" | tr ',' '\n')
+			do
+					iptables -A INPUT -s $IP -j DROP
+					echo " IP is blocked"
 					timeLimitfun
+			done
 		fi
 		# sleep 1
-	done
+
 done
