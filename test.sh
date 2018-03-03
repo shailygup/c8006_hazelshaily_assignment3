db3aea6ffa6dda00c9c6406470079fcc93ba22f4
Hazel Cha <hazel.cha@cloudreach.com>
2018-03-02 18:39:06 -0800

    random variable in test script

--- test.sh
+++ test.sh
@@ -3,11 +3,12 @@
 clear
 
 attackingIP=184.68.138.20,192.1689.0.4
-time=1
 
 echo 'Testing the monitoring Application ...'
 
 for IP in $(echo $attackingIP | tr ',' '\n')
 do
-  
+	time=$(($RANDOM % 10 + 1))
+	
+	sleep $time 
 done
